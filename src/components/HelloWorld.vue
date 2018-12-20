973a36628038494e45ab9972d44521fd341c7650
unknown <1909590616@qq.com>
2018-12-20 19:47:38 +0800

    loadingele

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <div>
+      <loading/>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+import loading from "@/components/loading";
+import api from './../axios/api.js'
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+     
+    };
+  },
+  components: {
+    loading
+  },
+  mounted() {
+    api.fzaxios("https://112.64.170.158:8190/Service1.svc/GetWaterworks")
+      .then(res => {
+        console.log(res);
+       
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
